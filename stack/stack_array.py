0cb965aac481ac7a3178434882adfbf0d76f1e6e
Anish Arya <anishcodes@hotmail.com>
2022-06-15 22:19:18 +0530

    made attributes as private

--- stack/stack_array.py
+++ stack/stack_array.py
@@ -17,16 +17,16 @@ class StackArray:
         '''In: 
             max_size: maximum size of the array
             stack_type: Element data type of the stack. e.g. int for Integer, str for String.'''
-        self.max_size = max_size
-        self.stack_type = stack_type
+        self.__max_size = max_size
+        self.__stack_type = stack_type
         self.__stack = array([None for _ in range(max_size)])
-        self.top = -1
+        self.__top = -1
 
     def size(self) -> int:
-        return self.top + 1
+        return self.__top + 1
     
     def isEmpty(self) -> bool:
-        if self.top == -1:
+        if self.__top == -1:
             return True
         return False
 
@@ -34,25 +34,25 @@ class StackArray:
         '''if returned value is -1, then the stack is empty'''
         if self.isEmpty():
             return -1
-        return self.top
+        return self.__top
 
     def push(self, obj):
         '''This function returns the status: OverflowError for failure (Overflow), 1 for success, and TypeError for not a homogeneous element.'''
-        if self.size() == self.max_size:
+        if self.size() == self.__max_size:
             raise CustomOverflowError
         ## check if the object entered is homogeneous 
-        if isinstance(obj, self.stack_type):
-            self.top += 1
-            self.__stack[self.top] = obj
+        if isinstance(obj, self.__stack_type):
+            self.__top += 1
+            self.__stack[self.__top] = obj
             return 1        
-        raise TypeError(f"TypeError: element pushed is not homogeneous. Element type should be of {self.stack_type}, was {type(obj)}")
+        raise TypeError(f"TypeError: element pushed is not homogeneous. Element type should be of {self.__stack_type}, was {type(obj)}")
         
     def pop(self):
         '''return UnderflowError for underflow, and return the popped element for success'''
         if self.isEmpty() is False:
-            popped_element = self.__stack[self.top]
-            self.__stack[self.top] = None
-            self.top -= 1
+            popped_element = self.__stack[self.__top]
+            self.__stack[self.__top] = None
+            self.__top -= 1
             return popped_element
         raise CustomUnderflowError
     
@@ -61,7 +61,7 @@ class StackArray:
 
 if __name__ == "__main__":
 
-    stack1 = StackArray(max_size=5, stack_type=int)
+    stack1 = StackArray(max_size=2, stack_type=int)
     print(f"Stack is: {stack1.peek()}")
 
     # size before inserting
@@ -99,8 +99,9 @@ if __name__ == "__main__":
     # pop
     try:
         print(f"popped element: {stack1.pop()}")
-        print(f"stack post pop: {stack1.peek()}")
     except CustomUnderflowError as ue:
         print(ue.message)
 
     print(f"stack post pop: {stack1.peek()}")
+
+    print(__file__)
